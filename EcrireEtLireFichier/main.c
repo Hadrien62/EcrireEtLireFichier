8aa31bd8f740daed97fec61be86142a8ce253ea3
Hadrien62 <hadrien.delobel@student.junia.com>
2023-09-01 20:57:41 +0200

    lire un fichier

--- EcrireEtLireFichier/main.c
+++ EcrireEtLireFichier/main.c
@@ -8,7 +8,16 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    FILE * fp;
+    fp = fopen("/Users/hadriendelobel/Documents/TPalgoFin/4villes.txt", "r");
+    if(fp == NULL){
+        printf("Erreur lors de l'ouverture du fichier\n");
+        return 1;
+    }
+    char c = fgetc(fp);
+    while(c != EOF){
+        printf("%c", c);
+        c = fgetc(fp);
+    }
     return 0;
 }
