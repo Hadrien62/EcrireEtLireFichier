ec64373587718cada722ca2ea576e556a19f9685
Hadrien62 <hadrien.delobel@student.junia.com>
2023-09-01 20:48:44 +0200

    Initial Commit

---
+++ EcrireEtLireFichier/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  EcrireEtLireFichier
+//
+//  Created by Hadrien Delobel on 01/09/2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
